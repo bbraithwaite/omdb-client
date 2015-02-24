256bb781479745c2cee286466c0d25912e330604
bbraithwaite <bradley.braithwaite@gmail.com>
2015-02-24 02:18:50 +0100

    added examples for get requests

---
+++ examples/get.example.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var imdbApi = require('../index');
+
+/**
+ * Find movie by title.
+ */
+var paramsWithTitle = {
+	title: 'Terminator',
+	year: 2012
+};
+
+imdbApi.get(paramsWithTitle, function(err, data) {
+	console.log(data);
+});
+
+/**
+ * Find movie by id.
+ */
+var paramsWithId = {
+	id: 'tt0276751'
+};
+
+imdbApi.get(paramsWithId, function(err, data) {
+	console.log(data);
+});
+
+/**
+ * Find series by title & type.
+ */
+var seriesWithOptions = {
+	title: 'Terminator',
+	type: 'series'
+};
+
+imdbApi.get(seriesWithOptions, function(err, data) {
+	console.log(data);
+});
+
+/**
+ * Find movies by title & type. Inlcude full plot and rotten tomatoes review.
+ */
+var movieWithOptions = {
+	title: 'Milk',
+	type: 'movie',
+	plot: 'full',
+	incTomatoes: true
+};
+
+imdbApi.get(movieWithOptions, function(err, data) {
+	console.log(data);
+});
